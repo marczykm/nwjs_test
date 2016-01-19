43c532024d182dce544ada4c87090b022e57b225
Marcin Marczyk <marcin.marczyk@capgemini.com>
2016-01-19 15:47:11 +0100

    refactored

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-C:\Users\mmarczyk\Downloads\nwjs-v0.12.3-win-x64\nw.exe C:\Users\mmarczyk\Downloads\nwjs_test
+C:\Users\mmarczyk\Downloads\nwjs-v0.12.3-win-x64\nw.exe C:\Users\mmarczyk\Downloads\nwjs_test --remote-debugging-port=8080
